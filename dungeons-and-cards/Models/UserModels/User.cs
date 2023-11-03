9aedeafcf156ec22eae7ac30cbfd0f8e788c9a7d
StrangerDeer <karmanb96@gmail.com>
2023-11-03 11:23:33 +0100

    user has password, can check and change password

--- dungeons-and-cards/Models/UserModels/User.cs
+++ dungeons-and-cards/Models/UserModels/User.cs
@@ -5,10 +5,21 @@ public class User : UserM
 {
     private static Guid Id { get; set; }
     private static DateTime StartDate { get; set; }
-    public User(string userName, string emailAddress) : base(Id, userName, emailAddress, StartDate)
+    private static string UserPassword { get; set; } = null!;
+    public User(string username, string password, string emailAddress) : base(Id, username, UserPassword, emailAddress, StartDate)
     {
         Id = Guid.NewGuid();
         StartDate = DateTime.Now;
+        UserPassword = BCrypt.Net.BCrypt.EnhancedHashPassword(password, 13);
     }
-    
+
+    public bool CheckPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.EnhancedVerify(password, Password);
+    }
+
+    public void ChangePassword(string newPassword)
+    {
+        Password = BCrypt.Net.BCrypt.EnhancedHashPassword(newPassword, 13);
+    }
 }
