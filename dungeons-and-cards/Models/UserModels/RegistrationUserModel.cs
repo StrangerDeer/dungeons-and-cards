099ded095616ad928b6bdf219a064a7578e524b9
StrangerDeer <karmanb96@gmail.com>
2023-11-07 12:14:24 +0100

    add RegistrationUserModel.cs

---
+++ dungeons-and-cards/Models/UserModels/RegistrationUserModel.cs
@@ -0,0 +1,15 @@
+﻿namespace dungeons_and_cards.Models.UserModels;
+
+public class RegistrationUserModel
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string EmailAddress { get; set; }
+
+    public RegistrationUserModel(string username, string password, string emailAddress)
+    {
+        Username = username;
+        Password = password;
+        EmailAddress = emailAddress;
+    }
+}
