84cea7732cc83fec4ba35f756b81ab0634628296
StrangerDeer <karmanb96@gmail.com>
2023-11-14 17:22:36 +0100

    add EmptyStringTypeChecker.cs

---
+++ dungeons-and-cards/Services/Validators/EmptyStringTypeChecker.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace dungeons_and_cards.Services.Validators;
+
+public class EmptyStringTypeChecker : BaseValidator
+{
+    private static readonly string EmptyStringFieldMessage = "One of the fields is empty";
+        
+    public EmptyStringTypeChecker() : base(EmptyStringFieldMessage)
+    {
+    }
+
+    public override bool Checker<T>(T parameter)
+    {
+        PropertyInfo[] properties = parameter!.GetType().GetProperties();
+        
+        foreach (PropertyInfo property in properties)
+        {
+            if (property.GetValue(parameter)!.ToString().IsNullOrEmpty())
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
