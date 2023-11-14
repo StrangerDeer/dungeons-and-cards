8166ea23924e4e41e108db7109f0bd79aae73939
StrangerDeer <karmanb96@gmail.com>
2023-11-14 17:23:06 +0100

    add BaseValidator.cs

---
+++ dungeons-and-cards/Services/Validators/BaseValidator.cs
@@ -0,0 +1,20 @@
+﻿using dungeons_and_cards.Services.Exceptions;
+
+namespace dungeons_and_cards.Services.Validators;
+
+public abstract class BaseValidator
+{
+    private string Message { get; set; }
+
+    protected BaseValidator(string message)
+    {
+        Message = message;
+    }
+    
+    public abstract bool Checker<T>(T parameter) where T : class;
+
+    public virtual void ValidatorMessage()
+    {
+        throw new BadRequestException(Message);
+    }
+}
